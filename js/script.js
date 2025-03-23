41698320fb049514aec68f7d810750f0ec75235b
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-23 22:32:20 +0000

    added index

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-// Copyright (c) 2020 Catherine All rights reserved
+// Copyright (c) 2025 Catherine All rights reserved
 //
 // Created by: Catherine Vu
-// Created on: March 23
+// Created on: March 23 2025
 // This file contains the JS functions for index.html
 
 /**
  * Calculates Length and Width of Rectangle 
  */
- function calculateArea() {
- }
-            let length = document.getElementById('length').value;
-            let width = document.getElementById('width').value;
-            
-            if(length && width) {
-                let area = length * width;
-                document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
-            }
+
+function calculatePerimeter() {
+    // Get length and width values
+    let length = document.getElementById('length').value;
+    let width = document.getElementById('width').value;
+    let result = document.getElementById('result');
+
+    // Check if length and width are provided and are valid numbers
+    if (length && width && !isNaN(length) && !isNaN(width)) {
+      // Convert values to numbers
+        length = parseFloat(length);
+        width = parseFloat(width);
+
+      // Calculate the perimeter of the rectangle
+      let perimeter = 2 * (length + width);
+
+      // Display the result
+        result.innerText = "Perimeter is: " + perimeter + " cm";
+    } else {
+      // If length or width is missing or invalid
+        result.innerText = "Please enter valid numbers for both length and width.";
+    }
+    }
+
