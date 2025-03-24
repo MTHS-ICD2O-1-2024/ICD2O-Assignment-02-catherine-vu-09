0fbe077ab54f913236abd1cd14124df2c646add6
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-24 17:13:31 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -14,21 +14,20 @@ function calculatePerimeter() {
     let width = document.getElementById('width').value;
     let result = document.getElementById('result');
 
-    // Check if length and width are provided and are valid numbers
-    if (length & width && !isNaN(length) && !isNaN(width)) {
-      // Convert values to numbers
-        length = parseFloat(length);
-        width = parseFloat(width);
+    // Convert values to numbers
+    length = parseFloat(length);
+    width = parseFloat(width);
 
-      // Calculate the perimeter of the rectangle
-      let perimeter = 2 * (length + width);
+    // Check if length and width are valid numbers
+    if (!isNaN(length) && !isNaN(width)) {
+        // Calculate the perimeter of the rectangle
+        let perimeter = 2 * (length + width);
 
-      // Display the result
+        // Display the result
         result.innerText = "Perimeter is: " + perimeter + " cm";
     } else {
-      // If length or width is missing or invalid
+        // If length or width is missing or invalid
         result.innerText = "Please enter valid numbers for both length and width.";
     }
-    
-    }
+}
 
